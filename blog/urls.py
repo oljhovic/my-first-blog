0399507c4451ec429031e98ecb93ffd484bf5826
mak <szao125480@gmail.com>
2018-02-20 08:10:38 +0300

    sort by -published date

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from . import views
+from views import current_datetime
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url('^time/$', current_datetime)
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Post
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
+import datetime
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def current_datetime(request):
+	now = datetime.datetime.now()
+	html = '<html><body> today is %s. </body></html>' % now
+	return HttpResponse(html)
+
+
