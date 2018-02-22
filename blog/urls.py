5ea46c2d950dcef2ce392047151ab01fec87a792
mak <szao125480@gmail.com>
2018-02-22 07:24:06 +0300

    new css

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url
 from . import views
-from views import current_datetime
+from views import current_datetime, hours_ahead, index
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
+    #url(r'^$', views.post_list, name='post_list'),
+    url(r'^$', views.index, name='index'),
+    url(r'^all_posts/$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
-    url('^time/$', current_datetime)
+    url('^time/$', current_datetime),
+    url(r'^time/plus/(\d{1,2})/$', hours_ahead),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from .models import Post
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
@@ -13,9 +13,28 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
+
+def index(request):
+	return render(request, 'blog/base.html')
+
+
+
+
+
+
 def current_datetime(request):
 	now = datetime.datetime.now()
 	html = '<html><body> today is %s. </body></html>' % now
 	return HttpResponse(html)
+
+def hours_ahead(request, offset):
+	try:
+		offset = int(offset)
+	except ValueError:
+		raise Http404()
+	dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+	html = '<html><body> cherez %s chasov budet %s. </body></html>' % (offset, dt)
+	return HttpResponse(html)
+
 
 
